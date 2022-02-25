f4d4d87183c20c68fed32c6fb7aadd0cd88a5026
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-25 01:21:31 -0600

    product table added

--- models/index.js
+++ models/index.js
@@ -3,10 +3,17 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsTo } = require('./Category');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
