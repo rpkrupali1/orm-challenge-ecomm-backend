2c157e59ed0a884425bbd2fce7cf2c93076ca3c6
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-25 03:18:22 -0600

    get and get all requests for category created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,42 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include:{
+      model: Product
+    }
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne(
+    {
+      where:{
+        id: req.params.id
+      },
+      include: {
+        model: Product
+      }
+    }
+  )
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No Category found with this id' });
+      return;
+    }
+    res.json(dbPostData)
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
